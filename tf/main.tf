42250419c8a04783c41eadd12d5fa93f07c0b4ba
vsahin <vsahin@consultant.ffn.com>
2021-11-13 16:46:43 +0300

    readme updated

---
+++ tf/main.tf
@@ -0,0 +1,82 @@
+resource "aws_vpc" "foo" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "ECS-EFS-VPC"
+  }
+}
+
+resource "aws_subnet" "alpha" {
+  vpc_id            =  aws_vpc.foo.id
+  availability_zone = "us-west-2a"
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "ECS-EFS-SUBNET"
+  }
+}
+resource "aws_efs_file_system" "efs" {
+  tags = {
+    Name = "ECS-EFS-FS"
+  }
+}
+
+resource "aws_efs_mount_target" "mount" {
+  file_system_id = aws_efs_file_system.efs.id
+  subnet_id      = aws_subnet.alpha.id
+  tags = {
+    Name = "ECS-EFS-MNT"
+  }
+}
+
+resource "aws_ecs_cluster" "ecs-bitcoin" {
+  name = "efs-example"
+}
+
+resource "aws_ecs_service" "bar" {
+  name            = "efs-example-service"
+  cluster         = aws_ecs_cluster.ecs-bitcoin.id
+  task_definition = aws_ecs_task_definition.efs-task.arn
+  desired_count   = 2
+  launch_type     = "EC2"
+
+  network_configuration {
+    subnets = [aws_subnet.alpha.id]
+  }
+}
+
+resource "aws_ecs_task_definition" "efs-task" {
+  family        = "efs-example-task"
+
+  container_definitions = <<DEFINITION
+[
+  {
+      "memory": 128,
+      "portMappings": [
+          {
+              "hostPort": 80,
+              "containerPort": 80,
+              "protocol": "tcp"
+          }
+      ],
+      "essential": true,
+      "mountPoints": [
+          {
+              "containerPath": "/bitcoin/.bitcoin",
+              "sourceVolume": "efs-data"
+          }
+      ],
+      "name": "docker-bitcoind",
+      "image": "nginx"
+  }
+]
+DEFINITION
+
+  volume {
+    name      = "efs-data"
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.efs.id
+      root_directory = "/bitcoin/.bitcoin"
+    }
+  }
+}
